171e7881ba21d9780ebebc9378864c17f65b3ba5
Tim Kinnel <tkinnel@gmail.com>
2013-03-25 17:16:07 -0300

    Update person.rb
    Moved collection identifier to params.

--- lib/google_plus/person.rb
+++ lib/google_plus/person.rb
@@ -36,7 +36,8 @@ module GooglePlus
     
     # List all the people whi this person has added to one or more circles. Limited to circles
     # the person has made visible to the requesting app. Does not return circle information.
-    def self.list(user_id, collection = 'public', params = {})
+    def self.list(user_id, params = {})
+      collection = params[:collection].nil? ? 'public' : params[:collection]
       data = make_request(:get, "people/#{user_id}/people/#{collection}", params)
       GooglePlus::Cursor.new(self, :get, 'people', params)
     end
